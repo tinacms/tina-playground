4aba85cedb465b515c4c73d865bb5cb540d8e112
Jeff See <jeffsee.55@gmail.com>
2021-12-14 12:24:42 -0800

    Remove headers from request

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,4 +1,7 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL =
+//   "https://tinacms-site-next-6pwid2q8w-tinacms.vercel.app/api/graphql";
+// // export const API_URL = "http://localhost:3002/api/graphql";
 
 export const fetcher = async ({
   schemaCode,
@@ -11,6 +14,15 @@ export const fetcher = async ({
   queryCode: string;
   variables: object;
 }) => {
+  const url = `${API_URL}?schema=${encodeURIComponent(
+    JSON.stringify(schemaCode)
+  )}&content=${encodeURIComponent(
+    markdownCode
+  )}&query=${queryCode}&variables=${encodeURIComponent(
+    JSON.stringify(variables)
+  )}`;
+  console.log(url);
+
   const res = await fetch(
     `${API_URL}?schema=${encodeURIComponent(
       JSON.stringify(schemaCode)
