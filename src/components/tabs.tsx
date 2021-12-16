1f1771f437da4ff69ca61aa0f18d8e4a7926d60a
Jeff See <jeffsee.55@gmail.com>
2021-12-15 17:32:37 -0800

    Use blue instead of indigo

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -24,7 +24,7 @@ export function Tabs({
         <select
           id="tabs"
           name="tabs"
-          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
           onChange={(e) => setActiveIndex(Number(e.target.value))}
           value={String(activeIndex)}
         >
@@ -47,7 +47,7 @@ export function Tabs({
                 onClick={() => setActiveIndex(i)}
                 className={classNames(
                   i === activeIndex
-                    ? "border-indigo-500 text-indigo-600"
+                    ? "border-blue-500 text-blue-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
                   "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
                 )}
